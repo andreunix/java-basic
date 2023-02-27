75145d5053abed5a2a25d45bcaeef4a2a4dc65a7
André Carvalho <andreunix@proton.me>
2023-02-26 22:54:50 -0400

    add

--- src/com/andreunix/datastructure/Main.java
+++ src/com/andreunix/datastructure/Main.java
@@ -9,7 +9,7 @@ public class Main {
     No<String> no2 = new No<>("Conteudo no2");
     no1.setProximoNo(no2);
 
-    No<String> no3 = new No<>("Conreudo no3");
+    No<String> no3 = new No<>("Conteudo no3");
     no2.setProximoNo(no3);
     
     No<String> no4 = new No<>("Conteudo no4");
